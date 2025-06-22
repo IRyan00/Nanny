436760a6e3560612c914b90030576b013892b849
IRyan00 <eymas.ryan@gmail.com>
2025-06-22 19:17:56 +0200

    fix jwt from local storage into cookies test

--- backEnd/server.js
+++ backEnd/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
